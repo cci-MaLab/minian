86073b4e7231fdefac0d8571522d27b99dc74cb8
Haoying Fu <rainbow_pet@163.com>
2023-06-26 01:37:21 -0400

    update peak visualization

--- post_minian/postprocessing.py
+++ post_minian/postprocessing.py
@@ -356,14 +356,19 @@ class FeatureExploration:
 
     def find_events_peak(self, a: np.ndarray) -> np.ndarray:
         a = a.copy()
-        for i, b in enumerate(a):
-            if (i < len(a)-1) and (b != a[i+1]):
-                a[i] = b
-            elif i == len(a)-1:
-                a[i]=b
-            else:
-                a[i] = 0
-        return a
+        res = np.zeros(np.shape(a))
+        for row,b in enumerate(a):
+            u,i,c=np.unique(b,return_index=True,return_counts= True)
+            for m in range(1,len(u)):                
+                res[row,(i[m]+c[m-1])]=u[m]
+            # for i, c in enumerate(b):
+            #     if (i < len(b)-1) and (c != b[i+1]):
+            #         b[i] = c
+            #     elif i == len(b)-1:
+            #         b[i] = c
+            #     else:
+            #         b[i] = 0
+        return res
 
 
     def normalize_events(self, a: np.ndarray) -> np.ndarray:
